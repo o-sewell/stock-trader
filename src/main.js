b042c2653eb96c0ef2bed23bd39ac9f540ae214b
Oliver Sewell <o.sewell@hotmail.com>
2017-07-24 12:24:56 +0100

    stock-trader

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+import App from './App.vue'
+import { routes } from './routes';
+import store from './store/store';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://stock-trader-db8d4.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '£' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
+
